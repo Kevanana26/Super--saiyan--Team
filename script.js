483591ba1131114dd196aabe3ccc73bb6dc835fa
Kevanana26 <525kpiccinich@frhsd.com>
2024-05-03 11:20:12 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const teamMembers = [
+    {
+        name: 'Son Goku',
+        position: 'Fighter',
+        skills: ['Fighting', 'Shooting Ki Blasts', 'Offense', 'Transformations'],
+        strengths: 'Physical Strength', 'Ki Sensing',
+        weaknesses: 'Defense', 'Overconfidence',
+        biography: 'Goku is considered one of the greatest fighters in anime history and has multiple attacks and transformations to his advantage'
+    },
+ 
+        {
+            name: 'Vegeta',
+            position: 'Fighter',
+            skills: ['Fighting', 'Shooting Ki Blasts', 'Offense', 'Transfromations'],
+            strengths: 'Physical Strength', 'Ki Sensing',
+            weaknesses: 'Defense', 'Overconfidence',
+            biography: 'Goku is considered one of the greatest fighters in anime history and has multiple attacks and transformations to his advantage'
+        },
+]
+
+function generateTeamCards (){
+    const teamCardsContainer = document.getElementById('teamCards')
+
+teamMembers.forEach
+
+}
